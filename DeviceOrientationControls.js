6265bb085aac54921717583c923590e7528a55c9
noze12 <13155037+noze12@users.noreply.github.com>
2021-12-24 00:05:03 +0900

    return orientaion change handling

--- DeviceOrientationControls.js
+++ DeviceOrientationControls.js
@@ -34,7 +34,7 @@
 			this.object.rotation.reorder( 'ZXY' );
 			this.enabled = true;
 			this.deviceOrientation = {};
-			//this.screenOrientation = 0;
+			this.screenOrientation = 0;
 			this.alphaOffset = 0; // radians
 
       // https://developers.google.com/web/updates/2016/03/device-orientation-changes
@@ -48,12 +48,12 @@
 
 			const onScreenOrientationChangeEvent = function () {
 
-				//scope.screenOrientation = window.orientation || 0;
+				scope.screenOrientation = window.orientation || 0;
 
 			}; // The angles alpha, beta and gamma form a set of intrinsic Tait-Bryan angles of type Z-X'-Y''
 
 
-			const setObjectQuaternion = function ( quaternion, alpha, beta, gamma ) {
+			const setObjectQuaternion = function ( quaternion, alpha, beta, gamma, orient ) {
 
 				_euler.set( beta, gamma, alpha, 'ZXY' );
 				//_euler.set( beta, -alpha, - gamma, 'YXZ' ); // 'ZXY' for the device, but 'YXZ' for us
@@ -63,7 +63,7 @@
 
 				// quaternion.multiply( _q1 ); // camera looks out the back of the device, not the top
 
-				//quaternion.multiply( _q0.setFromAxisAngle( _zee, - orient ) ); // adjust for screen orientation
+				quaternion.multiply( _q0.setFromAxisAngle( _zee, - orient ) ); // adjust for screen orientation
 
 			};
 
@@ -121,9 +121,9 @@
 
 					const gamma = device.gamma ? THREE.MathUtils.degToRad( device.gamma ) : 0; // Y''
 
-					//const orient = scope.screenOrientation ? THREE.MathUtils.degToRad( scope.screenOrientation ) : 0; // O
+					const orient = scope.screenOrientation ? THREE.MathUtils.degToRad( scope.screenOrientation ) : 0; // O
 
-					setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma );
+					setObjectQuaternion( scope.object.quaternion, alpha, beta, gamma, orient );
 
 					if ( 8 * ( 1 - lastQuaternion.dot( scope.object.quaternion ) ) > EPS ) {
 
